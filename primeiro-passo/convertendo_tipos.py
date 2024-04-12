01d26a7cc4eceba066bee4831834ceeb568fb135
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-12 15:26:48 -0300

    exemplor de como converter tipos

---
+++ primeiro-passo/convertendo_tipos.py
@@ -0,0 +1,11 @@
+# convertendo em int
+print(int(3.232)) 
+print(int("22"))
+
+# convertendo em float
+print(float("10.25"))
+print(float(200))
+
+# convertendo em string
+print(type(str(12.23)))
+print(type(str(50)))
