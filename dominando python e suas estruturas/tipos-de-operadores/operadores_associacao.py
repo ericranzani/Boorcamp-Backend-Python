f3806e387004cf5d63727141abcc5e5eef4b6c08
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-16 13:15:03 -0300

    término do modulo de operadores em python

---
+++ dominando python e suas estruturas/tipos-de-operadores/operadores_associacao.py
@@ -0,0 +1,8 @@
+curso = 'Curso de Python'
+frutas = ['laranja', 'uva', 'limão']
+saques = [400, 200]
+
+# Operador de associação "in"
+print("Python" in curso)
+print("maça" not in frutas)
+print(300 in saques)
