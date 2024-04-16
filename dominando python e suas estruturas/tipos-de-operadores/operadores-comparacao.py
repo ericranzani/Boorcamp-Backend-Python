9d3039dae5e7c8c130030574fbfade253812439a
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-16 11:52:59 -0300

    aperadores de comparação

---
+++ dominando python e suas estruturas/tipos-de-operadores/operadores-comparacao.py
@@ -0,0 +1,9 @@
+saldo = 450
+saque = 150
+
+print(saldo == saque) # igualdade
+print(saldo != saque) # diferente de
+print(saldo > saque) # maior que
+print(saldo >= saque) # maior que/ maior ou igual
+print(saldo < saque) # menor que
+print(saldo <= saque) # menor que/ menor ou igual
