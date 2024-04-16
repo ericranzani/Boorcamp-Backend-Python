bd7cc2a38652b659edb0057da2f301fa209f6ef8
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-16 12:03:38 -0300

    explicadao como é as operaçoes de atribuição

---
+++ dominando python e suas estruturas/tipos-de-operadores/operadores_atribuicao.py
@@ -0,0 +1,25 @@
+# Atribuição simples
+saldo = 550
+
+# Atribuição com adição 
+saldo += 300
+
+# Atribuição com subtração
+saldo -= 300
+
+# Atribuição com multipilcação
+saldo *= 2
+
+# Atribuição com divisão
+saldo /= 2
+
+# Atribuição com divisão inteira
+saldo //= 2
+
+# Atribuição com modulo
+saldo %= 2
+
+# Atribuição com exponenciação
+saldo **= 2
+
+print(saldo)
