ad9f8ba1b8281be3239755e8ada4be3c5be489dd
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-18 13:16:32 -0300

    aula sobre identação dentro do python

---
+++ estruturas condicionais e rep/indentacao_blocos.py
@@ -0,0 +1,9 @@
+def sacar(valor: float):
+    saldo = 500
+    if saldo >= valor:
+        print("valor sacado")
+    else:
+        print("Valor não permitido")
+print('Obrigado usar a nossa plataforma')
+
+sacar(600)
