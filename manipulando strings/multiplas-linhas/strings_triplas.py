cc27cf4a6ecd1f7f00a14af381c25fec3ec964f1
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-19 14:37:12 -0300

    ensinado como usar string triplas para fazul algum layout de mensagem

---
+++ manipulando strings/multiplas-linhas/strings_triplas.py
@@ -0,0 +1,24 @@
+nome = 'Eric'
+
+mensagem = f"""
+Olá meu nome é {nome}, 
+estou apredendo Python. 
+Essa mensagem tem diferentes recuos
+"""
+
+print(mensagem)
+
+
+print(
+    """
+    ############# MENU #############
+
+    1 - Depositar
+    2 - Sacar
+    3 - Sair
+
+    ################################
+
+   | Obrigado por usar nosso sistema |
+    """
+)
