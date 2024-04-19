c0f5e783f6e664f66df561853d250e970ffd9844
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-04-19 14:25:14 -0300

    demonstração das formas de fatiar ou trazer algum caracteres desejados da string

---
+++ manipulando strings/fatiamento-de-strings/fatiamento.py
@@ -0,0 +1,26 @@
+nome = 'Eric Ranzani de França'
+
+print(nome[0])
+
+# vai imprimir os caracteres ate o 8
+print(nome[:9])
+
+# pegar o ultimo indice
+print(nome[-1])
+
+# vai imprimir apartir do caracter 10 adiante
+print(nome[10:])
+
+# vai imprimir caracteres do indice 10 ate 16
+print(nome[10:16])
+
+# vai imprimir caracteres do indice 10 ate 16
+# com adição do step, ou seja, ele vai de 2 em 2
+print(nome[10:16:2])
+
+# traz a copia
+print(nome[:])
+
+# faz o espelhamento da variavel
+print(nome[::-1])
+
