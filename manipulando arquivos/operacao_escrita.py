28138b14eb3acb205dacf614b43b255695a22c1b
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-05-23 20:11:24 -0300

    aula sobre manipulações de arquivo, ler, escrever e gerenciar. Junto com manipulação de CSV

---
+++ manipulando arquivos/operacao_escrita.py
@@ -0,0 +1,4 @@
+arquivo = open('manipulando arquivos/teste.txt', 'w')
+arquivo.write("Escrevendo dados em um novo arquivo.")
+arquivo.writelines(["\n", "escrevendo", "\n", "um", "\n", "novo", "\n", "texto"])
+arquivo.close()
