eae546a354c23f0a46d60a6abc189192fe8c24a6
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-06-13 18:38:51 -0300

    aula demonstrativa de como inserir dados na tabela

--- Banco de dados/dbapi.py
+++ Banco de dados/dbapi.py
@@ -6,6 +6,12 @@ ROOT_PATH = Path(__file__).parent
 connect = sqlite3.connect(ROOT_PATH / "meu_banco.db")
 cursor = connect.cursor()
 
-cursor.execute(
-    "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))"
-)
+# criando a tabel e seus campos
+# cursor.execute(
+# "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))"
+# )
+
+# inserindo os dados em cada campo da tabela
+data = ("bruno", "bruno.ranzani20@gmail.com")
+cursor.execute("INSERT INTO clientes (nome, email) VALUES (?,?);", data)
+connect.commit()
