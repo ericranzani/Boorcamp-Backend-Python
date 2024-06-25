ecace772bbb9a678a599489275e3b31604278d5c
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-06-25 18:32:50 -0300

    aula do metodo alterar (update)  do banco de dados

--- Banco de dados/dbapi.py
+++ Banco de dados/dbapi.py
@@ -6,12 +6,27 @@ ROOT_PATH = Path(__file__).parent
 connect = sqlite3.connect(ROOT_PATH / "meu_banco.db")
 cursor = connect.cursor()
 
+
 # criando a tabel e seus campos
-# cursor.execute(
-# "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))"
-# )
+def criar_tabela(cursor):
+    cursor.execute(
+        "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))"
+    )
+    connect.commit()
+
 
 # inserindo os dados em cada campo da tabela
-data = ("bruno", "bruno.ranzani20@gmail.com")
-cursor.execute("INSERT INTO clientes (nome, email) VALUES (?,?);", data)
-connect.commit()
+def inserir_registro(connect, cursor, nome, email):
+    data = (nome, email)
+    cursor.execute("INSERT INTO clientes (nome, email) VALUES (?,?);", data)
+    connect.commit()
+
+
+# atualizando os dados em cada campo da tabela
+def atualizar_registro(connect, cursor, nome, email, id):
+    data = (nome, email, id)
+    cursor.execute("UPDATE clientes SET NOME=?, EMAIL=? WHERE id=?;", data)
+    connect.commit()
+
+
+atualizar_registro(connect, cursor, "Angelica", "angelica@gmail.com", 1)
